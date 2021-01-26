d10236b6812e5ddbf96baabbdd73be5d3f355f70
Filippo Massarelli <filippomassarelli@Filippos-MacBook-Pro-2.local>
2021-01-26 09:56:09 +0300

    fix issue with open path param in campaigns controller

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,7 +1,7 @@
 class Campaign < ApplicationRecord
     has_many :investments
 
-    scope :open, ->{ where("open == ?", true) }
+    scope :open, ->{ where(open: true) }
     scope :target_desc, ->{ order("target_amount DESC")}
     scope :target_asc, ->{ order("target_amount ASC")}
     # We can also use class methods instead of scopes
